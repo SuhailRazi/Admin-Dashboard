5cd1de7ee84048f6b0b90787deb79480347a43d9
Suhail Razi P M <suhailrazij@gmail.com>
2023-11-21 21:41:25 +0530

    Added folders for home, products,users, added react-router dom for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
+import Home from "./pages/home/Home"
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Users from "./pages/users/Users";
+import Products from "./pages/products/Products";
+
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <Home />
+      ),
+    },
+    {
+      path: "users",
+      element: <div><Users /></div>,
+    },
+    {
+      path: "products",
+      element: <div><Products /></div>,
+    },
+  ]);
+
   return (
-    <div>Hello World</div>
+    <RouterProvider router={router} />
   )
 }
 
