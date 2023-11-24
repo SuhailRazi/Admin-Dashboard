ea6036dcd5ddd3552ba268054cfb04bdd87b19fb
Suhail Razi P M <suhailrazij@gmail.com>
2023-11-24 22:45:44 +0530

    designed navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,7 +2,26 @@ import './navbar.scss';
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+      <div className="logo">
+        <img src='logo.svg' alt="" />
+        <span>Admin Dashboard</span>
+      </div>
+      <div className="icons">
+        <img src='/search.svg' alt='' className='icon' />
+        <img src='/app.svg' alt='' className='icon' />
+        <img src='/expand.svg' alt='' className='icon' />
+        <div className="notification">
+          <img src='/notifications.svg' alt='' />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img src='https://images.pexels.com/photos/18374442/pexels-photo-18374442/free-photo-of-flag-and-street-light-over-narrow-street.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt='' />
+          <span>username</span>
+        </div>
+        <img src='/settings.svg' alt='' className='icon' />
+      </div>
+    </div>
   )
 }
 
