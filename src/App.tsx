262c2d6493dc04925928336b418032495b8a4e06
Suhail Razi P M <suhailrazij@gmail.com>
2023-11-24 20:51:44 +0530

    Added uotlet for reusing the dashboard main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,60 @@
 import Home from "./pages/home/Home"
-import { createRoot } from "react-dom/client";
 import {
   createBrowserRouter,
   RouterProvider,
-  Route,
-  Link,
+  Outlet
 } from "react-router-dom";
 import Users from "./pages/users/Users";
 import Products from "./pages/products/Products";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Menu from "./components/menu/Menu";
+import Login from "./pages/login/Login";
 
 function App() {
 
+
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    )
+  }
+
+
   const router = createBrowserRouter([
     {
-      path: "/",
-      element: (
-        <Home />
-      ),
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <Home />
+        },
+        {
+          path: '/users',
+          element: <Users /> 
+        },
+        {
+          path: '/products',
+          element: <Products />
+        }
+      ]
     },
     {
-      path: "users",
-      element: <div><Users /></div>,
-    },
-    {
-      path: "products",
-      element: <div><Products /></div>,
-    },
+      path: '/login',
+      element: <Login />
+    }
   ]);
 
   return (
