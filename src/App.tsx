edf79b288d10281d2121c2377b6a82d30c3167a1
Suhail Razi P M <suhailrazij@gmail.com>
2023-11-24 21:31:41 +0530

    Added basic styles and sample design for the navbar and overall layout in global.scss

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Menu from "./components/menu/Menu";
 import Login from "./pages/login/Login";
+import './styles/global.scss';
 
 function App() {
 
