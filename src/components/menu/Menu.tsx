3053ba4dcb3657c08b3743d8459d202be46beb16
Suhail Razi P M <suhailrazij@gmail.com>
2023-11-25 22:12:03 +0530

    Styles menubar , imported data from data set and mapped over menu

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,8 +1,22 @@
+import { Link } from 'react-router-dom';
 import './menu.scss';
+import { menu } from '../../data';
 
 const Menu = () => {
   return (
-    <div>Menu</div>
+    <div className='menu'>
+      {menu.map((item) => (
+        <div className="item" key={item.id}>
+        <span className='title'>{item.title}</span>
+          {item && item.listItems.map((listItem) => (
+              <Link to={listItem.url} className='listItem'>
+                <img src={listItem.icon} alt=''/>
+                <span className='listItemTitle'>{listItem.title}</span>
+              </Link>
+          ))}
+        </div>
+      ))}
+    </div>
   )
 }
 
