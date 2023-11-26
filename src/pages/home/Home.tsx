55a871fa7429339d503577c5ef99677d9d06f867
Suhail Razi P M <suhailrazij@gmail.com>
2023-11-26 13:27:32 +0530

    called topbox function for box 1 in homepage

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,12 @@
+import TopBox from '../../components/topBox/TopBox'
 import './home.scss'
 
 const Home = () => {
   return (
     <div className="home">
-      <div className="box box1">Box 1</div>
+      <div className="box box1">
+        <TopBox />
+      </div>
       <div className="box box2">Box 2</div>
       <div className="box box3">Box 3</div>
       <div className="box box4">Box 4</div>
