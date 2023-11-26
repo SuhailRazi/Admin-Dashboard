f8f5ebbc1e82447a0d8a138617d6176f2ba1a4c8
Suhail Razi P M <suhailrazij@gmail.com>
2023-11-26 16:00:22 +0530

    called charbox function in respective boxes in hom page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,4 @@
+import ChartBox from '../../components/chartBox/ChartBox'
 import TopBox from '../../components/topBox/TopBox'
 import './home.scss'
 
@@ -7,11 +8,11 @@ const Home = () => {
       <div className="box box1">
         <TopBox />
       </div>
-      <div className="box box2">Box 2</div>
-      <div className="box box3">Box 3</div>
+      <div className="box box2"><ChartBox /></div>
+      <div className="box box3"><ChartBox /></div>
       <div className="box box4">Box 4</div>
-      <div className="box box5">Box 5</div>
-      <div className="box box6">Box 6</div>
+      <div className="box box5"><ChartBox /></div>
+      <div className="box box6"><ChartBox /></div>
       <div className="box box7">Box 7</div>
       <div className="box box8">Box 8</div>
       <div className="box box9">Box 9</div>
