3064ababf47fcc48b3536b60aa233ce096f19ae6
Suhail Razi P M <suhailrazij@gmail.com>
2023-11-26 13:27:03 +0530

    Menu section heading to uppercase

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -7,9 +7,9 @@ const Menu = () => {
     <div className='menu'>
       {menu.map((item) => (
         <div className="item" key={item.id}>
-        <span className='title'>{item.title}</span>
+        <span className='title'>{item.title.toUpperCase()}</span>
           {item && item.listItems.map((listItem) => (
-              <Link to={listItem.url} className='listItem'>
+              <Link to={listItem.url} className='listItem' key={listItem.id}>
                 <img src={listItem.icon} alt=''/>
                 <span className='listItemTitle'>{listItem.title}</span>
               </Link>
