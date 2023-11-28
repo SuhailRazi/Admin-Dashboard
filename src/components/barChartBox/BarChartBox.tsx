42e066a4721604d5f41c896e8146f939e0b16f6e
Suhail Razi P M <suhailrazij@gmail.com>
2023-11-29 02:16:56 +0530

    Added bar chart for 2 boxes in dashboard and called data ad props and passed as spread operator in home

---
+++ src/components/barChartBox/BarChartBox.tsx
@@ -0,0 +1,31 @@
+import { Bar, BarChart, ResponsiveContainer, Tooltip } from 'recharts'
+import './barChartBox.scss'
+
+type Props = {
+    color : string;
+    title : string;
+    dataKey : string;
+    chartData : object[];
+}
+
+const BarChartBox = (props : Props) => {
+  return (
+    <div className="barChartBox">
+        <h1>{props.title}</h1>
+        <div className="chart">
+            <ResponsiveContainer width="99%" height="100%">
+                <BarChart data={props.chartData}>
+                <Tooltip
+                    contentStyle={{ background: '#2a3447', borderRadius: '5px'}}
+                    labelStyle={{ display : 'none'}}
+                    cursor = {{fill: 'none'}}
+                />
+                <Bar dataKey={props.dataKey} fill={props.color} />
+                </BarChart>
+            </ResponsiveContainer>
+        </div>
+    </div>
+  )
+}
+
+export default BarChartBox
