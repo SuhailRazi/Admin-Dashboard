2bdf5ffa410fb704a13e94e8284c5b93059b9ebd
Suhail Razi P M <suhailrazij@gmail.com>
2023-11-29 02:18:17 +0530

    Called pie chart and bar chart component into the home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,8 @@
+import BarChartBox from '../../components/barChartBox/BarChartBox'
 import ChartBox from '../../components/chartBox/ChartBox'
+import PieChartBox from '../../components/pieChartBox/PieChartBox'
 import TopBox from '../../components/topBox/TopBox'
-import { chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser } from '../../data'
+import { barChartBoxRevenue, barChartBoxVisit, chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser } from '../../data'
 import './home.scss'
 
 const Home = () => {
@@ -11,12 +13,12 @@ const Home = () => {
       </div>
       <div className="box box2"><ChartBox {...chartBoxUser} /></div>
       <div className="box box3"><ChartBox {...chartBoxProduct}/></div>
-      <div className="box box4">Box 4</div>
+      <div className="box box4"><PieChartBox /></div>
       <div className="box box5"><ChartBox {...chartBoxRevenue}/></div>
       <div className="box box6"><ChartBox {...chartBoxConversion}/></div>
       <div className="box box7">Box 7</div>
-      <div className="box box8">Box 8</div>
-      <div className="box box9">Box 9</div>
+      <div className="box box8"><BarChartBox {...barChartBoxVisit} /></div>
+      <div className="box box9"><BarChartBox {...barChartBoxRevenue} /></div>
     </div>
   )
 }
