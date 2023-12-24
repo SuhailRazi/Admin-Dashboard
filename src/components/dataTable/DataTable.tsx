0ccee6037758423213b343aa98061f573044dbea
suhail <suhail.r@hubspire.com>
2023-12-24 18:13:37 +0530

    Converted data grid to reusable compnent and used JSOn from data to use it in isers table

--- src/components/dataTable/DataTable.tsx
+++ src/components/dataTable/DataTable.tsx
@@ -1,113 +1,18 @@
-import {
-  DataGrid,
-  GridColDef,
-  GridToolbar,
-  GridValueGetterParams,
-} from "@mui/x-data-grid";
+import { DataGrid, GridColDef, GridToolbar } from "@mui/x-data-grid";
 import "./dataTable.scss";
 
-const DataTable = () => {
-  const columns: GridColDef[] = [
-    { field: "id", headerName: "ID", width: 90 },
-    {
-      field: "avatar",
-      headerName: "Avatar",
-      width: 150,
-      renderCell: (params) => {
-        return <img src={params.row.img || "./noavatar.png"} alt="" />; //Render function to return any html element
-      },
-    },
-    {
-      field: "status",
-      headerName: "Status", // boolean value to show checkbox for true and false
-      width: 150,
-      type: "boolean",
-    },
-    {
-      field: "action",
-      headerName: "Action",
-      width: 150,
-      renderCell: (params) => {
-        return (
-          <div className="action">
-            <div className="view">View</div>
-            <div className="delete">Delete</div>
-          </div>
-        );
-      },
-    },
-    {
-      field: "firstName",
-      headerName: "First name",
-      width: 150,
-      editable: true,
-    },
-    {
-      field: "lastName",
-      headerName: "Last name",
-      width: 150,
-      editable: true,
-    },
-    {
-      field: "age",
-      headerName: "Age",
-      type: "number",
-      width: 110,
-      editable: true,
-    },
-    {
-      field: "fullName",
-      headerName: "Full name",
-      description: "This column has a value getter and is not sortable.",
-      sortable: false,
-      width: 160,
-      valueGetter: (params: GridValueGetterParams) =>
-        `${params.row.firstName || ""} ${params.row.lastName || ""}`,
-    },
-  ];
+type Props = {
+  columns: GridColDef[];
+  rows: object[];
+};
 
-  const rows = [
-    { id: 1, lastName: "Snow", firstName: "Jon", age: 14, status: true },
-    {
-      id: 2,
-      lastName: "Lannister",
-      firstName: "Cersei",
-      age: 31,
-      status: false,
-    },
-    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 31, status: true },
-    { id: 4, lastName: "Stark", firstName: "Arya", age: 11, status: false },
-    {
-      id: 5,
-      lastName: "Targaryen",
-      firstName: "Daenerys",
-      age: null,
-      status: true,
-    },
-    { id: 6, lastName: "Melisandre", firstName: null, age: 150, status: true },
-    {
-      id: 7,
-      lastName: "Clifford",
-      firstName: "Ferrara",
-      age: 44,
-      status: false,
-    },
-    {
-      id: 8,
-      lastName: "Frances",
-      firstName: "Rossini",
-      age: 36,
-      status: false,
-    },
-    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65, status: true },
-  ];
-
+const DataTable = (props: Props) => {
   return (
     <div className="dataTable">
       <DataGrid
         className="dataGrid"
-        rows={rows}
-        columns={columns}
+        rows={props.rows}
+        columns={props.columns}
         initialState={{
           pagination: {
             paginationModel: {
