997c8a280507c2763e09b31b2c321a6343a3b4d5
suhail <suhail.r@hubspire.com>
2023-12-24 18:50:29 +0530

    Made datagrid reusable and and passed slug fro users and created action buttons

--- src/components/dataTable/DataTable.tsx
+++ src/components/dataTable/DataTable.tsx
@@ -1,22 +1,46 @@
 import { DataGrid, GridColDef, GridToolbar } from "@mui/x-data-grid";
 import "./dataTable.scss";
+import { Link } from "react-router-dom";
 
 type Props = {
   columns: GridColDef[];
   rows: object[];
+  slug: string;
 };
 
 const DataTable = (props: Props) => {
+  const handleDelete = (id: number) => {
+    console.log(id + "has been deleted");
+  };
+
+  const actionColumn: GridColDef = {
+    field: "action",
+    headerName: "Action",
+    width: 200,
+    renderCell: (params) => {
+      return (
+        <div className="action">
+          <Link to={`/${props.slug}/${params.row.id}`}>
+            <img src="/view.svg" alt="" />
+          </Link>
+          <div className="delete" onClick={() => handleDelete(params.row.id)}>
+            <img src="/delete.svg" alt="" />
+          </div>
+        </div>
+      );
+    },
+  };
+
   return (
     <div className="dataTable">
       <DataGrid
         className="dataGrid"
         rows={props.rows}
-        columns={props.columns}
+        columns={[...props.columns, actionColumn]}
         initialState={{
           pagination: {
             paginationModel: {
-              pageSize: 5,
+              pageSize: 10,
             },
           },
         }}
