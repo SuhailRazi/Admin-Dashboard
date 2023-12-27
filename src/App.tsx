56ee14fe22e731ca9acf8221f45d7b3d8770b108
suhail <suhail.r@hubspire.com>
2023-12-27 16:44:41 +0530

    Added single product and single user url in routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
-import Home from "./pages/home/Home"
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Outlet
-} from "react-router-dom";
+import Home from "./pages/home/Home";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Users from "./pages/users/Users";
 import Products from "./pages/products/Products";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Menu from "./components/menu/Menu";
 import Login from "./pages/login/Login";
-import './styles/global.scss';
+import "./styles/global.scss";
+import User from "./pages/user/User";
+import Product from "./pages/product/Product";
 
 function App() {
-
-
   const Layout = () => {
     return (
       <div className="main">
@@ -29,38 +25,43 @@ function App() {
         </div>
         <Footer />
       </div>
-    )
-  }
-
+    );
+  };
 
   const router = createBrowserRouter([
     {
-      path: '/',
+      path: "/",
       element: <Layout />,
       children: [
         {
-          path: '/',
-          element: <Home />
+          path: "/",
+          element: <Home />,
         },
         {
-          path: '/users',
-          element: <Users /> 
+          path: "/users",
+          element: <Users />,
         },
         {
-          path: '/products',
-          element: <Products />
-        }
-      ]
+          path: "/products",
+          element: <Products />,
+        },
+        {
+          path: "/users/:id",
+          element: <User />,
+        },
+        {
+          path: "/products/:id",
+          element: <Product />,
+        },
+      ],
     },
     {
-      path: '/login',
-      element: <Login />
-    }
+      path: "/login",
+      element: <Login />,
+    },
   ]);
 
-  return (
-    <RouterProvider router={router} />
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
